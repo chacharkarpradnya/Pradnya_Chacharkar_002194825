180aaf2f12440478a12b92df8fb2dcc0060d3e0f
PRADNYA CHACAHRKAR <chacharkar.p@northeastern.edu>
2021-09-26 18:30:22 -0400

    Assignment 1

---
+++ AssignmentLab_1/src/Model/Product.java
@@ -0,0 +1,196 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.io.File;
+
+/**
+ *
+ * @author pradnyachacharkar
+ */
+public class Product {
+    
+   private String name;
+   private String geographicData;
+   private String dateOfBirth;
+   private String telephoneNum;
+   private String faxNum;
+   private String emailAdrss;
+   private String SSN;
+   private String medicalRcrdNum;
+   private String HealthPlanBeneficiaryNum;
+   private String BankAccNum;
+   private String certificateorlicenseNum;
+   private String vehicleIdentifiersAndSerialNum;
+   private String DeviceIdentifiersAndSerialNum;
+   private String linkedIn;
+   private String internetProtocolAddresses;
+   private String biometricIdentifiers;
+   private File photo;
+
+    public File getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(File photo) {
+        this.photo = photo;
+    }
+   private String uniqueIdentifyingNum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicData() {
+        return geographicData;
+    }
+
+    public void setGeographicData(String geographicData) {
+        this.geographicData = geographicData;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getTelephoneNum() {
+        return telephoneNum;
+    }
+
+    public void setTelephoneNum(String telephoneNum) {
+        this.telephoneNum = telephoneNum;
+    }
+
+    public String getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(String faxNum) {
+        this.faxNum = faxNum;
+    }
+
+    public String getEmailAdrss() {
+        return emailAdrss;
+    }
+
+    public void setEmailAdrss(String emailAdrss) {
+        this.emailAdrss = emailAdrss;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getMedicalRcrdNum() {
+        return medicalRcrdNum;
+    }
+
+    public void setMedicalRcrdNum(String medicalRcrdNum) {
+        this.medicalRcrdNum = medicalRcrdNum;
+    }
+
+    public String getHealthPlanBeneficiaryNum() {
+        return HealthPlanBeneficiaryNum;
+    }
+
+    public void setHealthPlanBeneficiaryNum(String HealthPlanBeneficiaryNum) {
+        this.HealthPlanBeneficiaryNum = HealthPlanBeneficiaryNum;
+    }
+
+    public String getBankAccNum() {
+        return BankAccNum;
+    }
+
+    public void setBankAccNum(String BankAccNum) {
+        this.BankAccNum = BankAccNum;
+    }
+
+    public String getCertificateorlicenseNum() {
+        return certificateorlicenseNum;
+    }
+
+    public void setCertificateorlicenseNum(String certificateorlicenseNum) {
+        this.certificateorlicenseNum = certificateorlicenseNum;
+    }
+
+    public String getVehicleIdentifiersAndSerialNum() {
+        return vehicleIdentifiersAndSerialNum;
+    }
+
+    public void setVehicleIdentifiersAndSerialNum(String vehicleIdentifiersAndSerialNum) {
+        this.vehicleIdentifiersAndSerialNum = vehicleIdentifiersAndSerialNum;
+    }
+
+    public String getDeviceIdentifiersAndSerialNum() {
+        return DeviceIdentifiersAndSerialNum;
+    }
+
+    public void setDeviceIdentifiersAndSerialNum(String DeviceIdentifiersAndSerialNum) {
+        this.DeviceIdentifiersAndSerialNum = DeviceIdentifiersAndSerialNum;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getInternetProtocolAddresses() {
+        return internetProtocolAddresses;
+    }
+
+    public void setInternetProtocolAddresses(String internetProtocolAddresses) {
+        this.internetProtocolAddresses = internetProtocolAddresses;
+    }
+
+    public String getBiometricIdentifiers() {
+        return biometricIdentifiers;
+    }
+
+    public void setBiometricIdentifiers(String biometricIdentifiers) {
+        this.biometricIdentifiers = biometricIdentifiers;
+    }
+
+    /*public String getFullFacePhotosAndComparableImages() {
+        return photo;
+    }
+
+    public void setFullFacePhotosAndComparableImages(String fullFacePhotosAndComparableImages) {
+        this.FullFacePhotosAndComparableImages = FullFacePhotosAndComparableImages;
+    }*/
+
+    public String getUniqueIdentifyingNum() {
+        return uniqueIdentifyingNum;
+    }
+
+    public void setUniqueIdentifyingNum(String uniqueIdentifyingNum) {
+        this.uniqueIdentifyingNum = uniqueIdentifyingNum;
+    }
+
+    public Object getImage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+
+   
+
+   
+
+   
